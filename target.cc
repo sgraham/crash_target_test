e08e90f1fa73020b1323ac52bad11e9467f9c6b0
Scott Graham <scottmg@chromium.org>
2016-04-18 12:21:09 -0700

    setevent in uef

--- target.cc
+++ target.cc
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <windows.h>
 
+HANDLE g_event;
+
 LONG WINAPI UnhandledExceptionHandler(EXCEPTION_POINTERS* exception_pointers) {
   (void)exception_pointers;
   fprintf(stderr, "in UnhandledExceptionHandler\n");
+  SetEvent(g_event);
+  fprintf(stderr, "called SetEvent\n");
   TerminateProcess(GetCurrentProcess(), 1);
   return EXCEPTION_CONTINUE_SEARCH;
 }
@@ -17,6 +21,10 @@ extern "C" __declspec(dllexport) LONG WINAPI SuicideThunk(void* data) {
 
 int main() {
   SetUnhandledExceptionFilter(UnhandledExceptionHandler);
+
+  // Test event to confirm calling SetEvent is OK.
+  g_event = CreateEvent(nullptr, true, false, nullptr);
+
   fprintf(stderr, "SetUEF\n");
   HMODULE dll = LoadLibrary("dll.dll");
   if (!dll)
